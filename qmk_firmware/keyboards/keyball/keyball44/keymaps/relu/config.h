5d0129674fd289e6ed41b933610140703f6ca995
ykawabata17 <yuya.kawabata@techouse.jp>
2024-06-07 22:00:49 +0900

    fix

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/relu/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/relu/config.h
@@ -20,6 +20,6 @@
 #define COMBO_TERM 80
 
 // マウスレイヤーの設定
-#define POINTING_DEVICE_AUTO_MOUSE_ENABLE // 有効化
-#define AUTO_MOUSE_DEFAULT_LAYER 5 // 切り替えるマウスレイヤー番号を指定
-#define AUTO_MOUSE_TIME 300 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
+// #define POINTING_DEVICE_AUTO_MOUSE_ENABLE // 有効化
+// #define AUTO_MOUSE_DEFAULT_LAYER 5 // 切り替えるマウスレイヤー番号を指定
+// #define AUTO_MOUSE_TIME 300 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
--- qmk_firmware/keyboards/keyball/keyball44/keymaps/relu/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/relu/keymap.c
@@ -71,8 +71,10 @@ void oledkit_render_info_user(void) {
 #endif
 
 // マウスレイヤーの有効化
+/*
 #ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
 void pointing_device_init_user(void) {
     set_auto_mouse_enable(true);
 }
 #endif
+*/
