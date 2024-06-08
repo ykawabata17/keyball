e9678de2cd8061edb5b7721d2b0de652dc3789d7
ykawabata17 <yuya.kawabata@techouse.jp>
2024-06-08 18:33:56 +0900

    add: default setting

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/default/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/default/config.h
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+/*
 #ifdef RGBLIGHT_ENABLE
 #    define RGBLIGHT_EFFECT_BREATHING
 #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
@@ -32,8 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGBLIGHT_EFFECT_ALTERNATING
 #    define RGBLIGHT_EFFECT_TWINKLE
 #endif
+*/
 
 #define TAP_CODE_DELAY 5
 
-#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 1
