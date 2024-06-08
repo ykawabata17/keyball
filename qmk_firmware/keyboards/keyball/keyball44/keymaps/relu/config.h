7a1b4ea7121f433a2729c7bde7e00a2bf7a43033
ykawabata17 <yuya.kawabata@techouse.jp>
2024-06-08 14:53:54 +0900

    fix: mouse layer

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/relu/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/relu/config.h
@@ -19,5 +19,5 @@
 
 // マウスレイヤーの有効化
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE // 有効化
-#define AUTO_MOUSE_DEFAULT_LAYER 5 // 切り替えるマウスレイヤー番号を指定
+#define AUTO_MOUSE_DEFAULT_LAYER 4 // 切り替えるマウスレイヤー番号を指定
 #define AUTO_MOUSE_TIME 300 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
