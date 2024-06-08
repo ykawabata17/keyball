2d21add5559e0636c1dac93acbd100d385f877c4
ykawabata17 <yuya.kawabata@techouse.jp>
2024-06-08 14:51:04 +0900

    add: mouse layer

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/relu/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/relu/config.h
@@ -14,3 +14,10 @@
 #define KEYBALL_CPI_DEFAULT 1100 // マウス速度 (default: 500)
 #define KEYBALL_SCROLL_DIV_DEFAULT 5 // スクロール速度 (default: 4)
 
+// コンボキーの同時押し判定時間
+#define COMBO_TERM 80
+
+// マウスレイヤーの有効化
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE // 有効化
+#define AUTO_MOUSE_DEFAULT_LAYER 5 // 切り替えるマウスレイヤー番号を指定
+#define AUTO_MOUSE_TIME 300 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
